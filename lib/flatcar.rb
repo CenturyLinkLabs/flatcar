e64379c182f0a97c56d54fe2c78afe8cc8f8772d
Laura Frank <ljfrank@gmail.com>
2015-07-21 15:30:58 -0500

    More setup

--- lib/flatcar.rb
+++ lib/flatcar.rb
@@ -1,5 +1,5 @@
 module Flatcar
 
-
+  puts 'hello world'
 
 end
--- lib/flatcar/version.rb
+++ lib/flatcar/version.rb
@@ -1,7 +1,3 @@
 module Flatcar
   VERSION = '0.0.1'.freeze unless defined?(Flatcar::VERSION)
-
-  def self.say_hello(i)
-    # code here
-  end
 end
